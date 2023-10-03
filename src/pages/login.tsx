5a7efe44c446c55e46a8e46fe1fc22af1775497a
Anthony Shannon <ashannon1990@gmail.com>
2023-10-02 19:37:08 -0600

    Initial commit

---
+++ src/pages/login.tsx
@@ -0,0 +1,17 @@
+'use client'
+
+import { signIn } from "next-auth/react";
+
+const Login = () => {
+
+    return (
+        <div className="flex flex-col items-center justify-center bg-black min-h-screen w-full">
+            <img className="w-52 mb-5" src="https://storage.googleapis.com/pr-newsroom-wp/1/2018/11/Spotify_Logo_CMYK_Green.png" alt="" />
+            <div>
+                <button className="bg-[#1ab26b] text-white p-5 rounded-full" onClick={() => signIn('spotify', { callbackUrl: '/' })}>Login with Spotify</button>
+            </div>
+        </div>
+    )
+}
+
+export default Login;
