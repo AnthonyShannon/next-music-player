cf3adc3c32c458ce4cc3f73a424c0452aa188c58
Anthony Shannon <ashannon1990@gmail.com>
2023-10-04 19:58:45 -0600

    Setting up middleware to only allow viewing homepage when logged in

---
+++ src/middleware.js
@@ -0,0 +1,23 @@
+import { getToken } from "next-auth/jwt"
+import { NextResponse } from "next/server"
+
+export async function middleware(req) {
+    // If user is logged in there will be a token
+    const token = await getToken({ req, secret: process.env.JWT_SECRET });
+
+    const { pathname } = req.nextUrl
+
+    if (pathname.includes('/api/auth') || token) {
+        return NextResponse.next();
+    }
+
+    if (pathname.startsWith("/_next")) {
+        return NextResponse.next();
+    }
+
+    if (!token && pathname !== '/login') {
+        return NextResponse.redirect(new URL('/login', req.url));
+    }
+
+    return NextResponse.next()
+}
