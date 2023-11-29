aea6046edca6cf6f5bd114c17feec6fde038b31e
Anthony Shannon <ashannon1990@gmail.com>
2023-11-28 17:26:07 -0700

    adding song info hook

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,7 @@
 import { getSession } from "next-auth/react"
 import Sidebar from "@/components/Sidebar";
 import Center from "@/components/Center";
+import Player from "@/components/Player"
 import { GetServerSidePropsContext } from "next";
 
 
@@ -11,6 +12,9 @@ export default function Home() {
         <Sidebar />
         <Center />
       </main>
+      <div className="sticky bottom-0">
+        <Player />
+      </div>
     </div>
   )
 }
